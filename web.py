5403f6d73da5858d688157e57a6236cb0f2b588f
KISHORE N <kishoremsd2597@gmail.com>
2026-03-29 02:22:25 -0400

    Add files via upload

---
+++ web.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+FLAG = "PSNACET{s3rv3r_s1d3_t3mpl4t3_1nj3ct10n}"
+
+@app.route("/")
+def home():
+    return '''
+        <h2>Template Renderer</h2>
+        <form action="/render">
+            <input type="text" name="name">
+            <button type="submit">Render</button>
+        </form>
+    '''
+
+@app.route("/render")
+def render():
+    name = request.args.get("name", "")
+    template = f"Hello {name}!"
+    return render_template_string(template)
+
+if __name__ == "__main__":
+    app.run()
